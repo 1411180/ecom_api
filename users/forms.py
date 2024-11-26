a5f8daacc2ef51750bc2ccd3bd00abb394043900
1411180 <vkiragu752@gmail.com>
2024-11-26 10:57:29 +0300

    Add files via upload

---
+++ users/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Payment, Product
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['card_number', 'cardholder_name', 'expiry_date', 'cvv', 'amount']
+        widgets = {
+            'expiry_date': forms.TextInput(attrs={'type': 'date'}),
+            'cvv': forms.PasswordInput(render_value=False, attrs={'maxlength': 3}),
+        }
+
+    def clean_card_number(self):
+        card_number = self.cleaned_data.get('card_number')
+        if not card_number.isdigit() or len(card_number) != 16:
+            raise forms.ValidationError("Enter a valid 16-digit card number.")
+        return card_number
+
+    def clean_cvv(self):
+        cvv = self.cleaned_data.get('cvv')
+        if not cvv.isdigit() or len(cvv) != 3:
+            raise forms.ValidationError("Enter a valid 3-digit CVV.")
+        return cvv
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'image', 'video']
