cf33954ee2aae06e401cae2af79bd529fba1dd1f
1411180 <vkiragu752@gmail.com>
2024-11-27 10:42:38 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -5,9 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
-    video = models.FileField(upload_to='product_videos/', blank=True, null=True)
-
+   
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Cart")
     product = models.ForeignKey("Product", on_delete=models.CASCADE)
